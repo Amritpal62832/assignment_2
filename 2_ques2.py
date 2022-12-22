6cc49b27658b89b2cb6a987cf96d862485af54c7
Amritpal62832 <121246365+Amritpal62832@users.noreply.github.com>
2022-12-22 22:27:36 +0530

    Add files via upload

---
+++ 2_ques2.py
@@ -0,0 +1,5 @@
+name = "Amritpal singh"
+SID = 22104119
+department_name = "Electrical Engineering"
+CGPA= 9.9
+print(f"Hey,{name} Here!\nMy SID is {SID} \nI am from {department_name} department and my CGPA is {CGPA}")
